7e5d51ccd73fa9b038071b0f3ec7f3bb923c1d2e
Ash Moran <ash.moran@patchspace.co.uk>
2012-10-12 15:50:37 +0100

    Sell when price drops below limit (no delay yet)

--- spec/trailing_stop_loss_spec.rb
+++ spec/trailing_stop_loss_spec.rb
@@ -2,23 +2,34 @@ require 'spec_helper'
 
 class TrailingStopLoss
   def initialize(attributes)
-
+    @limit  = attributes[:limit]
+    @market = attributes[:market]
   end
 
   def price_changed(new_price)
-
+    if new_price < @limit
+      @market.sell
+    end
   end
 end
 
 describe "Trailing Stop Loss" do
   subject(:order) { TrailingStopLoss.new(limit: 9, market: self) }
 
+  # Interface for self-shunt
+  module Market
+    def sell
+      @actions << :sell
+    end
+
+    def actions
+      @actions
+    end
+  end
+  include Market
+
   before(:each) do
     @actions = [ ]
-  end
-
-  def actions
-    @actions
   end
 
   context "price goes up" do
@@ -32,6 +43,17 @@ describe "Trailing Stop Loss" do
     it "does not sell" do
       order.price_changed(9)
       expect(actions).to be_empty
+    end
+  end
+
+  context "price goes below the limit" do
+    it "sells" do
+      order.price_changed(8)
+      expect(actions).to be == [ :sell ]
+    end
+
+    it "doesn't do this if the blip is temporary" do
+      pending
     end
   end
 
