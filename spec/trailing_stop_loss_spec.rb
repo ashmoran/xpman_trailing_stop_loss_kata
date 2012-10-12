f3088f1bafab6b0652e16a81824338f5b886be61
Ash Moran <ash.moran@patchspace.co.uk>
2012-10-12 15:42:50 +0100

    Noop implementation to get test structure in place

--- spec/trailing_stop_loss_spec.rb
+++ spec/trailing_stop_loss_spec.rb
@@ -1,6 +1,33 @@
 require 'spec_helper'
 
+class TrailingStopLoss
+  def initialize(attributes)
+
+  end
+
+  def price_changed(new_price)
+
+  end
+end
+
 describe "Trailing Stop Loss" do
+  subject(:order) { TrailingStopLoss.new(limit: 9, market: self) }
+
+  before(:each) do
+    @actions = [ ]
+  end
+
+  def actions
+    @actions
+  end
+
+  context "price goes up" do
+    it "does not sell" do
+      order.price_changed(11)
+      expect(actions).to be_empty
+    end
+  end
+
   it "wipes the evil grin from @kevinrutherford's face" do
     pending
   end
