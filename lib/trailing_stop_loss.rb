19026b2e3ed427c463dd3d99f0bec002f780fcd0
Ash Moran <ash.moran@patchspace.co.uk>
2012-10-12 18:32:01 +0100

    Add a delay to acting on the sell order when the price drops

--- lib/trailing_stop_loss.rb
+++ lib/trailing_stop_loss.rb
@@ -8,10 +8,10 @@ class TrailingStopLoss
   def price_changed(price_info)
     new_price, price_time = price_info.values_at(:price, :time)
 
-    if new_price < @limit
+    if new_price < @limit && time_is_sufficient_to_trigger_sell?(price_time)
       @market.sell
     end
-    if new_price > @limit && time_is_sufficient_for_limit_raise?(price_time)
+    if new_price > @limit && time_is_sufficient_to_raise_limit?(price_time)
       @limit = new_price - 1
     end
 
@@ -20,7 +20,11 @@ class TrailingStopLoss
 
   private
 
-  def time_is_sufficient_for_limit_raise?(price_time)
+  def time_is_sufficient_to_trigger_sell?(price_time)
+    (price_time - @last_known_time) > 30
+  end
+
+  def time_is_sufficient_to_raise_limit?(price_time)
     (price_time - @last_known_time) > 15
   end
 end
--- spec/trailing_stop_loss_spec.rb
+++ spec/trailing_stop_loss_spec.rb
@@ -24,6 +24,20 @@ describe "Trailing Stop Loss" do
   }
 
   shared_examples_for "a price increase" do
+    it_behaves_like "a still-pending order"
+  end
+
+  shared_examples_for "an insignificant drop" do
+    it_behaves_like "a still-pending order"
+  end
+
+  shared_examples_for "a sell order" do
+    it "sells" do
+      expect(actions).to be == [ :sell ]
+    end
+  end
+
+  shared_examples_for "a still-pending order" do
     it "does not sell" do
       expect(actions).to be_empty
     end
@@ -76,26 +90,30 @@ describe "Trailing Stop Loss" do
     end
   end
 
-  context "price goes down to the limit" do
-
-  end
-
   context "price goes down" do
     context "to the limit" do
-      it "does not sell" do
+      when_event_happens do
         order.price_changed(price: 9, time: Float::INFINITY)
-        expect(actions).to be_empty
       end
+
+      it_behaves_like "an insignificant drop"
     end
 
     context "below the limit" do
-      it "sells" do
-        order.price_changed(price: 8, time: Float::INFINITY)
-        expect(actions).to be == [ :sell ]
+      context "for a significant time" do
+        when_event_happens do
+          order.price_changed(price: 8, time: 131)
+        end
+
+        it_behaves_like "a sell order"
       end
 
-      it "doesn't do this if the blip is temporary" do
-        pending
+      context "momentarily" do
+        when_event_happens do
+          order.price_changed(price: 8, time: 130)
+        end
+
+        it_behaves_like "a still-pending order"
       end
     end
   end
